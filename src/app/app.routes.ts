01f8140d7d465c539a167d06ef4ff5af7bfcae10
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-07 12:29:21 +0100

    feat: fleshed up home, added register and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: "", component: HomeComponent, title:"Faculty of Family Medicine App, West African College of Physicians" }
+  { path: "", component: HomeComponent, title:"Faculty of Family Medicine App, West African College of Physicians" },
+  { path: "access/register" }
 ];
