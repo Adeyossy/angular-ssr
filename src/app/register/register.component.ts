8c580bc80d131e682d44a1557ac59e0de1234345
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-09 13:39:14 +0100

    feat: adding validation to registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,12 +14,55 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   email = '';
+  emailFeedback = 'No email entered';
+  emailCorrect = false;
   password = '';
+  passwordFeedback = '';
+  passwordCorrect = false;
+  confirmPassword = '';
+  confirmPasswordFeedback = '';
+  confirmPasswordCorrect = false;
   userCredential$ = new Observable<UserCredential>();
 
   constructor(private authService: AuthService) { }
 
   signUp() {
     this.userCredential$ = this.authService.signUp$(this.email, this.password);
+  }
+
+  checkPassword(password: string) {
+    this.password = password;
+    if (password.length < 8) {
+      this.passwordCorrect = false;
+      this.passwordFeedback = "Your password is short. Passwords must have at least 8 characters";
+      return;
+    } else {
+      if (!/\d+/.test(password)) {
+        this.passwordCorrect = false;
+        this.passwordFeedback = "Password must contain at least one number";
+        return;
+      }
+
+      if (!/[A-Z]/.test(password)) {
+        this.passwordCorrect = false;
+        this.passwordFeedback = "Password must contain at least one capital letter"
+        return;
+      }
+
+      this.passwordCorrect = true;
+      this.passwordFeedback = "Password is very strong";
+    }
+    this.comparePasswords(this.confirmPassword);
+  }
+
+  comparePasswords(password: string) {
+    this.confirmPassword = password;
+    if (this.confirmPassword === this.password) {
+      this.confirmPasswordFeedback = "Passwords match";
+      this.confirmPasswordCorrect = true;
+    } else {
+      this.confirmPasswordFeedback = "Passwords do not match";
+      this.confirmPasswordCorrect = false;
+    }
   }
 }
